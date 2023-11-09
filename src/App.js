82bb5605b1f51861325c89c63863f0cd05590213
1thorn1 <kjhi4691@naver.com>
2023-11-09 11:05:04 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
+const customers = [{ //customer 객체 정의
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '강주희', //속성 키 : 값
+  'birthday' : '980605',
+  'gender' : '여자',
+  'job' : '머학생'
+},
+
+{ //customer 객체 정의
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '강강', //속성 키 : 값
+  'birthday' : '19980605',
+  'gender' : '여자',
+  'job' : '인턴'
+},
+
+{ //customer 객체 정의
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '쏜팡이', //속성 키 : 값
+  'birthday' : '20231109',
+  'gender' : '여자',
+  'job' : '쏜덕'
+}]
+
+function App(props) {
   return (
-    <div className="gray-background">
-      <img src ={logo} lat="logo"></img>
-      <h2>let's develop management system</h2>
+    <div>
+      {
+        customers.map( c => { //배열 함수
+        return(
+          <Customer
+          key = {c.id} //id 값이 다 다르므로 설정, key 반드시 필요
+          id = {c.id}
+          image = {c.image}
+          name = {c.name}
+          birhtday = {c.birthday}
+          gender = {c.gender}
+          job = {c.job}/>
+        );
+      })
+    }
     </div>
-  );
-}
+  )}
+    
 
 export default App;
---
+++ src/components/Customer.js
@@ -0,0 +1,49 @@
+/*
+import React from 'react'; //라이브러리 가져오기
+
+class Customer extends React.Component{ //react에 component 형태로 작성이된 클래스 이는 일종의 라이브러리이자 클래스
+    // 이를 상속받아 하나의 컴포넌트 정의 가능
+    render(){ //실제로 그려지는 내용
+        <div>
+            <h2>강주희</h2>
+            <p>201834701</p>
+            <p></p>
+            <p>대학생</p>
+        </div>
+    } 
+}
+export default Customer; //Customer 클래스 내보내기
+*/
+
+import React from 'react';
+
+function Customer(props) { //props를 직접 담아 사용
+    return (
+        <div>
+            <CustomerProfile id = {props.id} image = {props.image} name = {props.name}/>
+            <CustomerInfo birthday = {props.birthday} gender = {props.gender} job = {props.job}/>
+        </div> //jsx 문법 중 하나 , 빠지면 error
+    );
+}
+//Customer profile과 Customer inform으로 구조화 시키기
+
+function CustomerProfile(props){
+    return(
+        <div>
+            <img src ={props.image} alt="profile"/>
+            <h2>{props.name}({props.id})</h2>
+        </div>
+    );
+}
+
+function CustomerInfo(props){
+    return(
+        <div>
+            <p>{props.birthday}</p>
+            <p>{props.gender}</p>
+            <p>{props.job}</p>
+        </div>
+    )
+}
+
+export default Customer;
